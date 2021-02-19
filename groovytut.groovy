b5a36d34397dc9dd5e5c99866b366863a8b588a4
battosai-slayer691 <vashtienasheldy@gmail.com>
2021-02-19 12:40:30 -0600

    2nd part of the exercises

--- groovytut.groovy
+++ groovytut.groovy
@@ -11,12 +11,12 @@ class GroovyTut{
         println("5 / 4 = " + (5 / 4));
         println("5 / 4 = " + (5.intdiv(4)));
         println("5 % 4 = " + (5 % 4));
-        println("5 + 4 = " + (5.2 + 4.3));
+        println("5.5 + 4.6 = " + (5.5 + 4.6));
         
         println("5.2 + 4.4 = " + (5.2.plus(4)));
-        println("5 + 4 = " + (5..2.minus(4)));
-        println("5 + 4 = " + (5.2.multiply(4.4)));
-        println("5 + 4 = " + (5.2 / 4.4));
+        println("5 - 4 = " + (5.2.minus(4)));
+        println("5 * 4 = " + (5.multiply(4)));
+        println("5 / 4 = " + (5 / 4));
         
         println("age++ " + (age++));
         println("++age " + (++age));
