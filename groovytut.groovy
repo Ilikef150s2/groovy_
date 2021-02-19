0e3554e2bce1e22e290336ec15e04aabd1cce944
Ilikef150s2 <79163122+Ilikef150s2@users.noreply.github.com>
2021-02-19 12:49:54 -0500

    Update groovytut.groovy

--- groovytut.groovy
+++ groovytut.groovy
@@ -2,6 +2,15 @@ class GroovyTut{
     static void main(String[] args){
 
         println("Hello world");
+        def age = "Dog";
+        age = 40;
+
+        println("5 + 4 = " + (5 + 4));
+        println("5 - 4 = " + (5 - 4));
+        println("5 * 4 = " + (5 * 4));
+        println("5 / 4 = " + (5 / 4));
+        println("5 / 4 = " + (5.intdiv(4)));
+        println("5 % 4 = " + (5 % 4));
         
     }
 }
